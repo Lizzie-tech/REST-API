9ed3b38f4c274823dc62448bfd6d96cc5916b11c
Lizzie-tech <elizabethnjobvu@gmail.com>
2022-06-03 22:03:22 +0100

    added the update user path and put a http exception status code

--- main.py
+++ main.py
@@ -45,3 +45,21 @@ async def delete_user(user_id: UUID):
         status_code=404,
         detail=f"user with id: {user_id} does not exist"
     )
+
+@app.put("/api/v1/users/{users_id}")
+async def update_user(user_update: UpdateUser, user_id: UUID):
+    for user in db:
+        if user.id == user_id:
+            if user_update.first_name is not None:
+                user.first_name = user_update.first_name
+            if user_update.last_name is not None:
+                user.last_name = user_update.last_name
+            if user_update.middle_name is not None:
+                user.middle_name = user_update.middle_name
+            if user_update.roles is not None:
+                user.roles = user_update.roles
+            return
+    rasise HTTPException(
+        status_code=404,
+        detail=f"user with id: {user_id} does not exist"
+    )
